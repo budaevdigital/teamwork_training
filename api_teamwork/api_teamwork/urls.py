0e82f7f2504ff7c5e1e59a8493954a5a5812a41f
paracosm17 (Aleksandr Popov) <paracosm17@aol.com>
2022-07-31 22:31:33 +1000

    Add redoc

--- api_teamwork/api_teamwork/urls.py
+++ api_teamwork/api_teamwork/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("users.urls"))
+    path('', include("users.urls")),
+    path(
+        'redoc/',
+        TemplateView.as_view(template_name='redoc.html'),
+        name='redoc'
+    ),
 ]
